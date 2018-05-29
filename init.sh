bc1b4f57b3ef01d2cedf0dd2e43157f59e69259c
Dean Woods <dmwoods38@gmail.com>
2018-05-28 23:16:04 -0700

    Added some more packages.

--- init.sh
+++ init.sh
@@ -11,9 +11,11 @@ apt-get install -y firmware-iwlwifi
 modprobe -r iwlwifi; modprobe iwlwifi
 apt-get install -y vim xorg firefox-esr net-tools curl wget git i3 dnsutils
 apt-get install -y network-manager-gnome network-manager ranger scrot imagemagick 
+apt-get install -y exfat-utils psmisc
 
 # Appearance stuff
 apt-get install -y lxappearance feh rofi neofetch i3blocks compton
+apt-get install -y fonts-font-awesome arandr blackbird-gtk-theme xfonts-terminus
 
 # Grab configs from git
 git clone https://github.com/dmwoods38/dotfiles.git
