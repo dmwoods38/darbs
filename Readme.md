4330fd77ece130e5df8713fa2a1c554619678e7c
Dean Woods <dmwoods38@gmail.com>
2018-05-29 22:13:56 -0700

    Removed proprietary software and non-free repo. Removed software that isn't needed right now with MATE system.

--- Readme.md
+++ Readme.md
@@ -1,5 +1,6 @@
 # DARBS
 Bootstrapping scripts for Debian install, the name is a reference to Luke Smith's LARBS.
 Definitely a work in progress.
+After upgrading the wifi card to one that has free software support now using debian without the non-free repos. Removed some packages as well as I am now using MATE w/i3.
 
 
--- init.sh
+++ init.sh
@@ -4,18 +4,11 @@ apt-get update && apt-get upgrade -y
 apt-get install -y sudo software-properties-common
 usermod -aG sudo $USER
 
-# Sorry RMS
-apt-add-repository non-free
-apt-get update
-apt-get install -y firmware-iwlwifi
-modprobe -r iwlwifi; modprobe iwlwifi
-apt-get install -y vim xorg firefox-esr net-tools curl wget git i3 dnsutils
-apt-get install -y network-manager-gnome network-manager ranger scrot imagemagick 
-apt-get install -y exfat-utils psmisc
+apt-get install -y vim net-tools curl wget git i3 dnsutils
 
 # Appearance stuff
-apt-get install -y lxappearance feh rofi neofetch i3blocks compton
-apt-get install -y fonts-font-awesome arandr blackbird-gtk-theme xfonts-terminus
+apt-get install -y neofetch i3blocks
+apt-get install -y fonts-font-awesome xfonts-terminus
 
 # Grab configs from git
 git clone https://github.com/dmwoods38/dotfiles.git
