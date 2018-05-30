130994f8401bd434dfe41c60c39b1a0ec0f3fec6
Dean Woods <dmwoods38@gmail.com>
2018-05-29 23:10:47 -0700

    Added golang installation and dep installation.

--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 USER=dmwoods
+GOVERSION=1.10.2
+ARCH=amd64
 apt-get update && apt-get upgrade -y
 apt-get install -y sudo software-properties-common
 usermod -aG sudo $USER
 
-apt-get install -y vim net-tools curl wget git i3 dnsutils
+apt-get install -y vim net-tools curl wget git i3 dnsutils vim-pathogen dconf-editor
 
 # Appearance stuff
 apt-get install -y neofetch i3blocks
@@ -13,8 +15,21 @@ apt-get install -y fonts-font-awesome xfonts-terminus
 # Grab configs from git
 git clone https://github.com/dmwoods38/dotfiles.git
 cp -r dotfiles/.* /home/$USER
-chown -R $USER:$USER /home/$USER
+
+# Install go
+wget https://dl.google.com/go/go$GOVERSION.linux-$ARCH.tar.gz
+tar -C /usr/local -xzf go$GOVERSION.linux-$ARCH.tar.gz
+
+# Install dep
+sh dep_install.sh
 
 # Cleanup files
 rm -rf dotfiles
 rm /home/$USER/Readme.md
+rm go$GOVERSION.linux-$ARCH.tar.gz
+
+
+# install vim-go
+mkdir -p /home/$USER/.vim/bundle
+git clone https://github.com/fatih/vim-go.git /home/$USER/.vim/bundle/vim-go 
+chown -R $USER:$USER /home/$USER
